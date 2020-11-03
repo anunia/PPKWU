b179ddc8f5edad3825f21742cb4d969f52c7ea84
Aneta Pawelec <215843@edu.p.lodz.pl>
2020-11-03 15:04:36 +0100

    Add uppercase counter

--- Zdanie 2 - text analysis/API.py
+++ Zdanie 2 - text analysis/API.py
@@ -3,6 +3,9 @@ import flask
 app = flask.Flask(__name__)
 
 @app.route('/analysis/<text>', methods=['GET'])
+def analysis(text):
+	uppercase = sum(map(str.isupper, text))
 
+    return 
      
 app.run()
