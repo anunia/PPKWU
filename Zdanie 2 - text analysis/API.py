d15ff35c7859f712ed617f049c06df31bba8dab6
Aneta Pawelec <215843@edu.p.lodz.pl>
2020-11-03 15:20:10 +0100

    Change counter for special characters

--- Zdanie 2 - text analysis/API.py
+++ Zdanie 2 - text analysis/API.py
@@ -7,13 +7,13 @@ def analysis(text):
 	uppercase = sum(map(str.isupper, text))
 	lowercase = sum(map(str.islower, text))
 	number = sum(map(str.isdigit, text))
-	special = len(text) - len( re.findall('[\w]', text))
-	result = {
+	special = sum(not ch.isalnum() for ch in text)
+	result2 = {
 		"Number of uppercase characters: " : uppercase,
 		"Number of lowercase characters: " : lowercase,
 		"Number of number characters: " : number,
 		"Number of special characters: " : special}
 
-    return result
-     
+	return result2
+
 app.run()
