5b59620dedfed8af63a346044e2d11bf719a1f78
Aneta Pawelec <215843@edu.p.lodz.pl>
2020-11-03 15:05:38 +0100

    Add number counter

--- Zdanie 2 - text analysis/API.py
+++ Zdanie 2 - text analysis/API.py
@@ -6,7 +6,7 @@ app = flask.Flask(__name__)
 def analysis(text):
 	uppercase = sum(map(str.isupper, text))
 	lowercase = sum(map(str.islower, text))
-
+	number = sum(map(str.isdigit, text))
     return 
      
 app.run()
