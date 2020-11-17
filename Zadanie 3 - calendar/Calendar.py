fdc3d018e879e7ac3e75ae11223397b7a0cf36ac
Aneta Pawelec <215843@edu.p.lodz.pl>
2020-11-17 15:19:01 +0100

    Define function get_info

--- Zadanie 3 - calendar/Calendar.py
+++ Zadanie 3 - calendar/Calendar.py
@@ -1,11 +1,16 @@
 import flask
 import ics
 import bs4
+from calendar import month
 
 app = flask.Flask(__name__)
 
 @app.route('/calendar/<year>/<month>', methods=['GET'])
 def calendar(year,month):
+    get_info(year, month)
     return 0
-                 
+          
+def get_info(year, month):  
+    
+    return 0     
 app.run()
