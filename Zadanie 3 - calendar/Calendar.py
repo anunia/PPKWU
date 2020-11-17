2a6f6c018140aa5bf93178d4a1e8521ffb4d0ce7
Aneta Pawelec <215843@edu.p.lodz.pl>
2020-11-17 15:52:15 +0100

    Get calendar info from http://www.weeia.p.lodz.pl/

--- Zadanie 3 - calendar/Calendar.py
+++ Zadanie 3 - calendar/Calendar.py
@@ -1,16 +1,19 @@
 import flask
 import ics
 import bs4
+from urllib.request import urlopen
 from calendar import month
 
 app = flask.Flask(__name__)
 
 @app.route('/calendar/<year>/<month>', methods=['GET'])
 def calendar(year,month):
-    get_info(year, month)
-    return 0
+    ret = get_info(year, month)
+    return ret
           
 def get_info(year, month):  
-    
-    return 0     
+    url = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=" + str(year) + "&miesiac=" + str(month) + "&lang=1"
+    info = request.urlopen(url)
+    print(info.prettify())
+    return info     
 app.run()
