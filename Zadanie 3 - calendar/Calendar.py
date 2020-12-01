aba21f6f9277af1914ce880364d5ab2473d970c7
Aneta Pawelec <215843@edu.p.lodz.pl>
2020-12-01 14:43:25 +0100

    Set selectors

--- Zadanie 3 - calendar/Calendar.py
+++ Zadanie 3 - calendar/Calendar.py
@@ -1,19 +1,31 @@
 import flask
 import ics
-import bs4
+from bs4 import BeautifulSoup
 from urllib.request import urlopen
 from calendar import month
+import datetime
 
 app = flask.Flask(__name__)
 
 @app.route('/calendar/<year>/<month>', methods=['GET'])
 def calendar(year,month):
     ret = get_info(year, month)
-    return ret
+    return flask.jsonify(ret)
+
+@app.route("/events")
+def calendar_current_month():
+    current_month = datetime.datetime.now()
+    return jsonify(get_info(current_month.year, current_month.month))
           
 def get_info(year, month):  
-    url = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok=" + str(year) + "&miesiac=" + str(month) + "&lang=1"
-    info = request.urlopen(url)
-    print(info.prettify())
-    return info     
+    url = "http://www.weeia.p.lodz.pl/pliki_strony_kontroler/kalendarz.php?rok={}&miesiac={}&lang=1".format(year, month)
+    event_selector = "#kalendarz td.active"
+    title_selector = "#kalendarz .InnerBox"
+    day_and_url_selector = "#kalendarz a"
+    
+    info = urlopen(url)
+    html = BeautifulSoup(info.read())
+    
+    return html  
+   
 app.run()
