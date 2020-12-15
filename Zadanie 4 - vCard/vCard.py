02d83357d869e0b8dffc21d7b46dc8ac4612dcfe
Aneta Pawelec <215843@edu.p.lodz.pl>
2020-12-15 15:53:24 +0100

    Add address and phone number selector.

--- Zadanie 4 - vCard/vCard.py
+++ Zadanie 4 - vCard/vCard.py
@@ -10,8 +10,14 @@ def contact(category,location):
 
 def get_info(category, location):  
     url = "https://panoramafirm.pl/szukaj?k={}y&l={}".format(category, location)
-    event_selector = "#company-list card.company-item.py-2.container.my-2"
-    title_selector = "#company-list "
+    list_selector = "#company-list .card.company-item.py-2.container.my-2"
+    name_selector = "#company-list [title = \"Zobacz informacje szczegółowe o firmie\"]"
+    address_selector = "#company-list .address"
+    number_selector = "#company-list .icon-telephone.addax.addax-cs_hl_phonenumber_click"
+
+    info = urlopen(url)
+    html = BeautifulSoup(info.read())
+    selected_elements = html.select(list_selector)
 
     return 0  
    
