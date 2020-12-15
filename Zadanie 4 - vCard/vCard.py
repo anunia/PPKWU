674b18bc7a81fc1e0d87f857103a5066bfd49563
Aneta Pawelec <215843@edu.p.lodz.pl>
2020-12-15 15:01:37 +0100

    Add /contact/<category>/<location> endpoint

--- Zadanie 4 - vCard/vCard.py
+++ Zadanie 4 - vCard/vCard.py
@@ -3,4 +3,9 @@ import flask
 
 app = flask.Flask(__name__)
 
+@app.route('/contact/<category>/<location>', methods=['GET'])
+def contact(category,location):
+    result = get_info(category, location)
+    return 0
+
 app.run()
