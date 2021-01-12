789a0e9f95530b8877810200b60f397971832bed
Aneta Pawelec <215843@edu.p.lodz.pl>
2021-01-12 14:34:46 +0100

    Create selector for url and email

--- Zadanie 4 - vCard/vCard.py
+++ Zadanie 4 - vCard/vCard.py
@@ -1,8 +1,11 @@
 import flask
-
+import vcard
 
 app = flask.Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def hrllo():
+    return "Hello"
 @app.route('/contact/<category>/<location>', methods=['GET'])
 def contact(category,location):
     result = get_info(category, location)
@@ -14,11 +17,18 @@ def get_info(category, location):
     name_selector = "#company-list [title = \"Zobacz informacje szczegółowe o firmie\"]"
     address_selector = "#company-list .address"
     number_selector = "#company-list .icon-telephone.addax.addax-cs_hl_phonenumber_click"
+    url_selector = "#company-list .icon-website.addax addax-cs_hl_hit_homepagelink_click"
+    email_selector = "#company-list .ajax-modal-link.icon-envelope cursor-pointer.addax addax-cs_hl_email_submit_click"
 
     info = urlopen(url)
     html = BeautifulSoup(info.read())
     selected_elements = html.select(list_selector)
+    
+    c = []
 
+    for contact in selected_elements:
+       c
+        
     return 0  
    
 app.run()
